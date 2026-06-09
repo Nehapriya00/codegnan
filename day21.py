dc41329fde204ff475c78f49ca0f82074293d9be
Neha Priya <126246648+Nehapriya00@users.noreply.github.com>
2026-06-09 16:02:56 +0530

    Add files via upload

--- day21.py
+++ day21.py
@@ -1,10 +1,9 @@
 #university management system using oops
 class Person:
     university_name="Codegnan University"
-    def __init__(self,name,age,depart,gender):
+    def __init__(self,name,age,gender):
         self.name=name
-        self.age=age
-        self.depart=depart
+        self.age=age        
         self.gender=gender
     def display(self):
         pass
@@ -12,8 +11,9 @@ class Person:
 class Student(Person):
     student_count=0
     def __init__(self,name,age,studentID,depart,year,gender,course):
-        super().__init__(name,age,depart,gender)
+        super().__init__(name,age,gender)
         self.studentID=studentID
+        self.depart=depart
         self.year=year
         self.course=course
         Student.student_count+= 1
@@ -34,8 +34,9 @@ std.display()
 class Faculty(Person):
     faculty_count=0
     def __init__(self,name,age,depart,gender,facultyID,education):
-        super().__init__(name,age,depart,gender)
+        super().__init__(name,age,gender)
         self.facultyID=facultyID
+        self.depart=depart
         self.education=education
         Faculty.faculty_count+=1
     def display(self):
@@ -51,61 +52,90 @@ fac.display()
 
 class Watchman(Person):
     watchman=0
-    def __init__(self,name,age,depart,gender,ID):
-        super().__init__(name,age,depart,gender)
-        self.gender=gender
+    def __init__(self,name,age,gender,ID):
+        super().__init__(name,age,gender)                
         self.ID=ID
         Watchman.watchman+=1
     def display(self):
         print("-------WatchMan Details--------")
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Department: {self.depart}")
+        print(f"Age: {self.age}")        
         print(f"Gender: {self.gender}")
         print(f"WatchMan ID: {self.ID}")
-watch=Watchman("Raju",67,"cse","Female","HTD98567")
+watch=Watchman("Raju",67,"Female","HTD98567")
 watch.display()
 
 class Helper(Person):
     helper=0
-    def __init__(self,name,age,depart,gender,ID):
-        super().__init__(name,age,depart,gender)
-        self.gender=gender
+    def __init__(self,name,age,gender,ID):
+        super().__init__(name,age,gender)                
         self.ID=ID
         Helper.helper+=1
     def display(self):
         print("-------Helper Details--------")
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Department: {self.depart}")
+        print(f"Age: {self.age}")        
         print(f"Gender: {self.gender}")
         print(f"Helper ID: {self.ID}")
-h=Helper("Roja",50,"cse","Female","GHT87654")
+h=Helper("Roja",50,"Female","GHT87654")
 h.display()
 
 class Driver(Person):
     driver=0
-    def __init__(self,name,age,depart,gender,ID,Salary):
-        super().__init__(name,age,depart,gender)
-        self.gender=gender
+    def __init__(self,name,age,gender,ID,Salary):
+        super().__init__(name,age,gender)                
         self.ID=ID
         self.Salary=Salary
         Driver.driver+=1
     def display(self):
         print("-------Driver Details--------")
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Department: {self.depart}")
+        print(f"Age: {self.age}")        
         print(f"Gender: {self.gender}")
         print(f"Driver ID: {self.ID}")
         print(f"Salary: {self.Salary}")
-d=Driver("Krishna",56,"college","Male","IKF78654",30000)
+d=Driver("Krishna",56,"Male","IKF78654",30000)
 d.display()
-    
-    
+
+class labassistance(Person):
+    lab=0
+    def __init__(self,name,age,gender,education,ID):
+        super().__init__(name,age,gender)
+        self.education=education
+        self.ID=ID
+        labassistance.lab+=1
+    def display(self):
+        print("-------labassistance--------")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")        
+        print(f"Gender: {self.gender}")
+        print(f"Lab Assistance ID: {self.ID}")
+        print(f"Education: {self.education}")
+l=labassistance("Prema",24,"FEMALE",897654,"BTech")
+l.display()
+
+class Non_teaching(Person):
+    non=0
+    def __init__(self,name,age,gender,education,ID):
+        super().__init__(name,age,gender)
+        self.education=education
+        self.ID=ID
+        Non_teaching.non+=1
+    def display(self):
+        print("-------Non Teaching Staff--------")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")        
+        print(f"Gender: {self.gender}")
+        print(f"Lab Assistance ID: {self.ID}")
+        print(f"Education: {self.education}")
+n=Non_teaching("Krishna",45,"MALE",67543,"BTech")
+n.display()
+           
 print("---------Total Counts------------")
 print("Total Students",Student.student_count)
 print("Total Faculty",Faculty.faculty_count)
 print("Total Watchman",Watchman.watchman)
 print("Total Helpers",Helper.helper)
 print("Total Drivers",Driver.driver)
+print("Total Labassistance",labassistance.lab)
+print("Total NON-Teaching Staff",Non_teaching.non)
